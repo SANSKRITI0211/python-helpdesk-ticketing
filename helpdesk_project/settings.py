0a15265054ecf103ed410c58181b9d1db1871fcf
SANSKRITI0211 <sanskritidewda02@gmail.com>
2026-07-02 17:29:53 +0530

    Allow Render hostnames in settings.py

--- helpdesk_project/settings.py
+++ helpdesk_project/settings.py
@@ -19,13 +19,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$08z=&rbv)3=gzdc+h%j0pypyu@#h70au(pr17=zx14p9ydfy'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-c$08z=&rbv)3=gzdc+h%j0pypyu@#h70au(pr17=zx14p9ydfy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+allowed_hosts_env = os.environ.get('ALLOWED_HOSTS')
+if allowed_hosts_env:
+    for host in allowed_hosts_env.split(','):
+        ALLOWED_HOSTS.append(host.strip())
 
 
 # Application definition
